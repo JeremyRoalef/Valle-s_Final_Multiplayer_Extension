6d412913684cae4c20a3feac1358efb521d7ddac
JeremyRoalef <roalef944@morrisville.edu>
2026-02-02 10:07:18 -0500

    Finalize Networking Fundamentals Testing

--- Multiplayer_Test_Project/Assets/ClientPlayerMove.cs
+++ Multiplayer_Test_Project/Assets/ClientPlayerMove.cs
@@ -32,7 +32,39 @@ public class ClientPlayerMove : NetworkBehaviour
             //Enable movement
             playerInput.enabled = true;
             starterAssetsInputs.enabled = true;
+        }
+        //The server now has final say in the player movement
+        if (IsServer)
+        {
             thirdPersonController.enabled = true;
         }
+    }
+
+    //Take note on the naming convention used for the method, and the Unity Attribute used above the method. Any RPC method must end with Rpc.
+    //When using an Rpc in this manner, this networkbehaviour script does exist on both the client and the server, but it will only execute on the server
+    [Rpc(SendTo.Server)]
+    private void UpdateInputServerRpc(Vector2 move, Vector2 look, bool jump, bool sprint)
+    {
+        //Since the server has authority over the network transform, calling the methods that move the player on the server's end will move the player
+        //To understand how exactly the movement works, click on one of the methods and press f12 to open the location of the method in the StarterAssetsInputs.cs script
+        starterAssetsInputs.MoveInput(move);
+        starterAssetsInputs.LookInput(look);
+        starterAssetsInputs.JumpInput(jump);
+        starterAssetsInputs.SprintInput(sprint);
+
+    }
+
+    private void LateUpdate()
+    {
+        //If this is called on a client that doesn't own this behaviour, return early
+        if (!IsOwner) return;
+
+        //Tell the server to move the player
+        UpdateInputServerRpc(
+            starterAssetsInputs.move, 
+            starterAssetsInputs.look, 
+            starterAssetsInputs.jump, 
+            starterAssetsInputs.sprint
+            );
     }
 }
